ef5a5ac71855a8c8713d98c6ac071f971e4ebfb5
BabaGanoush <server.vbs@gmail.com>
2023-07-27 12:52:18 +0300

    add role

--- backend/models/user.model.js
+++ backend/models/user.model.js
@@ -17,7 +17,12 @@ const userSchema = new Schema({
     name: {
         type: String,
         required: true
-    }
+    },
+    role: {
+        type: String,
+        required: true
+    },
+
 })
 
 
