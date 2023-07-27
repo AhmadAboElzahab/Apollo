e23ffc7f3a1b4c9387da95f4dd884be133e17f2b
BabaGanoush <server.vbs@gmail.com>
2023-07-27 12:19:15 +0300

    remove

--- backend/routes/user.route.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-router.get('/', (req, res) => {
-    const hi = 'Hello, world!';
-    res.json({ hi });
-});
-
-module.exports = router;
