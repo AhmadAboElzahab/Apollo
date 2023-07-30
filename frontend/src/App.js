13e75f97ab4f2613f05b90b16bfabdd7523b3ac0
BabaGanoush <server.vbs@gmail.com>
2023-07-30 16:35:38 +0300

    Adding to landingpage

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,19 @@
-function App() {
-  return (
-    <div className="bg-slate-500">
-      <h1>welcome</h1>
+import Navbar from './Components/Navbar'
+import apollo from './assets/apollo.png'
 
+export default function App() {
+  return (
+    <div class="background">
+
+      <span></span>
+      <span></span>
+      <span></span>
+
+      <div className=" mx-auto w-screen  h-screen ">
+        <Navbar />
+        <img src={apollo} alt="" className="bottom-0 main_image mx-auto " width="650px" /></div>
     </div>
-  );
+  )
 }
 
-export default App;
+
