ead35da8e440109909e64b190cde43d4b93062ca
BabaGanoush <server.vbs@gmail.com>
2023-08-18 15:22:54 +0300

    editing styles

--- frontend/src/layout/HomeLayout.js
+++ frontend/src/layout/HomeLayout.js
@@ -2,11 +2,9 @@ import { Outlet } from 'react-router-dom';
 import Navbar from '../Components/Navbar';
 export default function HomeLayout() {
   return (
-    <div className='background'>
-      <div className='mx-auto w-screen h-screen flex flex-col justify-between'>
-        <Navbar />
-        <Outlet />
-      </div>
+    <div className='mx-auto w-screen h-screen flex flex-col justify-between'>
+      <Navbar />
+      <Outlet />
     </div>
   );
 }
