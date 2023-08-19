b1133ebf2a3357a9899bf5061864d5a90ccee1fb
BabaGanoush <server.vbs@gmail.com>
2023-08-19 19:21:08 +0300

    fixing layout

--- frontend/src/Components/ShopNavbar.js
+++ frontend/src/Components/ShopNavbar.js
@@ -1,7 +1,45 @@
+import { BsThreeDotsVertical } from 'react-icons/bs';
+
 export default function ShopNavbar() {
   return (
-    <div>
-      <div className='w-screen lg:w-96 lg:h-screen fixed lg:border-r border-b lg:py-5 lg:px-10  bg-black border-gray-900 '></div>
-    </div>
+    <nav className='bg-black/20 fixed top-[60px] lg:top-20  py-2 lg:border-r lg:backdrop-blur-0 backdrop-blur-sm border-b border-gray-900 w-screen lg:w-[30vh]'>
+      <div className='mx-auto flex items-center justify-between '>
+        <input
+          type='text'
+          placeholder='Search'
+          className='px-2 py-3 bg-black mx-3 w-full text-white border-gray-900 rounded outline-none focus:ring-0 '
+        />
+        <label
+          htmlFor='shopHamburger'
+          className='peer-checked:shopHamburger relative z-10  block cursor-pointer lg:hidden text-white text-center'
+        >
+          <BsThreeDotsVertical size={30} className='mr-4 text-gray-800 hover:text-white' />
+        </label>
+      </div>
+      <input
+        type='checkbox'
+        name='shopHamburger'
+        id='shopHamburger'
+        className='peer invisible absolute'
+        hidden
+      />
+
+      <div className='peer-checked:block hidden lg:block lg:h-full'>
+        <div className='h-screen flex flex-col px-3 p-5'>
+          <a href='#' className='text-white'>
+            Home
+          </a>
+          <a href='#' className='text-white'>
+            About
+          </a>
+          <a href='#' className='text-white'>
+            Services
+          </a>
+          <a href='#' className='text-white'>
+            Contact
+          </a>
+        </div>
+      </div>
+    </nav>
   );
 }
--- frontend/src/layout/ShopLayout.js
+++ frontend/src/layout/ShopLayout.js
@@ -1,48 +1,15 @@
 import { Outlet } from 'react-router-dom';
 import ShopNavbar from '../Components/ShopNavbar';
-import ShopHeader from '../Components/ShopHeader';
-import { BsThreeDotsVertical } from 'react-icons/bs';
 
 export default function ShopLayout() {
   return (
-    <nav className='bg-black fixed top-16 lg:top-20 pt-2 border-r border-gray-900 w-screen lg:w-[30vh]'>
-      <div className='mx-auto flex items-center justify-between '>
-        <input
-          type='text'
-          placeholder='Search'
-          className='px-2 py-3 bg-black mx-3 w-full text-white border-gray-900 rounded outline-none focus:ring-0 '
-        />
-        <label
-          htmlFor='shopHamburger'
-          className='peer-checked:shopHamburger relative z-10  block cursor-pointer lg:hidden text-white text-center'
-        >
-          <BsThreeDotsVertical size={30} className='mr-4' />
-        </label>
-      </div>
-      <input
-        type='checkbox'
-        name='shopHamburger'
-        id='shopHamburger'
-        className='peer invisible'
-        hidden
-      />
-
-      <div className='peer-checked:block hidden  lg:block lg:h-full'>
-        <div className='h-screen flex flex-col px-3 p-5'>
-          <a href='#' className='text-white'>
-            Home
-          </a>
-          <a href='#' className='text-white'>
-            About
-          </a>
-          <a href='#' className='text-white'>
-            Services
-          </a>
-          <a href='#' className='text-white'>
-            Contact
-          </a>
+    <div className='text-white'>
+      <ShopNavbar />
+      <div className='p-2 lg:pl-72 '>
+        <div className='mx-auto max-w-8xl pt-40 lg:pt-55   lg:py-20 space-y-8 px-2  lg:px-8 '>
+          <Outlet />
         </div>
       </div>
-    </nav>
+    </div>
   );
 }
