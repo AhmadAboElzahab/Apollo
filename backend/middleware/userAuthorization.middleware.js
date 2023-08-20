5d8c82c71b359a459b6b64e6e21a9651e4221161
BabaGanoush <server.vbs@gmail.com>
2023-08-20 13:30:06 +0300

    fixing the middlewares

--- backend/middleware/userAuthorization.middleware.js
+++ backend/middleware/userAuthorization.middleware.js
@@ -1,28 +1,22 @@
 const jwt = require('jsonwebtoken');
 
-
 const userAuthorization = (req, res, next) => {
-    const token = req.header('Authorization');
-
-    if (!token) {
-        return res.status(401).json({ message: 'Authorization token missing' });
+  const token = req.cookies.token;
+  if (!token) {
+    return res.status(401).json({ message: 'Authorization token missing' });
+  }
+  try {
+    const decoded = jwt.verify(token, process.env.SECRET_KEY);
+    req.user = decoded;
+    if (req.user.role !== 'user') {
+      return res.status(403).json({ message: 'Access denied. Users are allowed only.' });
     }
-
-    try {
-        const decoded = jwt.verify(token, process.env.SECRET_KEY);
-        req.user = decoded;
-
-        if (req.user.role !== 'user') {
-            return res.status(403).json({ message: 'Access denied. Users are  allowed Only.' });
-        }
-
-        req.userId = req.user.id;
-
-        next();
-    } catch (err) {
-        console.error(err);
-        res.status(401).json({ message: 'Invalid token' });
-    }
+    req.userId = req.user.id;
+    next();
+  } catch (err) {
+    console.error(err);
+    res.status(401).json({ message: 'Invalid token' });
+  }
 };
 
 module.exports = userAuthorization;
