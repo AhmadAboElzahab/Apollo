e56e6f0015cf5c7491a9f2bebb61ab5db7bd744f
BabaGanoush <server.vbs@gmail.com>
2023-08-28 23:57:44 +0300

    clening up

--- frontend/src/Components/ShopNavbar.js
+++ frontend/src/Components/ShopNavbar.js
@@ -10,34 +10,9 @@ export default function ShopNavbar() {
   return (
     <nav className='fixed top-[60px] z-[10] w-screen border-b  border-gray-900 bg-black/90 lg:bg-black/20 py-2 lg:top-20 lg:w-72 lg:border-r '>
       <div className='mx-auto flex items-center justify-between '>
-        <div className='relative  w-full px-2 '>
-          <div className='pointer-events-none absolute inset-y-0 left-6 flex  items-center'>
-            <svg
-              className='h-4 w-4 text-gray-500 '
-              aria-hidden='true'
-              xmlns='http://www.w3.org/2000/svg'
-              fill='none'
-              viewBox='0 0 20 20'
-            >
-              <path
-                stroke='currentColor'
-                strokeLinecap='round'
-                strokeLinejoin='round'
-                strokeWidth='2'
-                d='m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z'
-              />
-            </svg>
-          </div>
-
-          <input
-            type='text'
-            placeholder='Search'
-            className='block w-full rounded  border-gray-900 bg-black  py-3 pl-10 text-white'
-          />
-        </div>
         <label
           htmlFor='shopHamburger'
-          className='peer-checked:shopHamburger relative z-10  block cursor-pointer text-center text-white lg:hidden'
+          className='peer-checked:shopHamburger relative z-10  block cursor-pointer text-center text-white lg:hidden ml-auto'
         >
           <BsThreeDotsVertical size={30} className='mr-4 text-white' />
         </label>
